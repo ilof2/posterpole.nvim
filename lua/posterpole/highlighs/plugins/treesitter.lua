84ea45a5a9687b5ba3a90e4fc8890e7ab843d5fa
Illia <39670317+ilof2@users.noreply.github.com>
2025-04-04 11:33:06 +0200

    Fixed error with empty groups + treesitter for python (#16)

--- lua/posterpole/highlighs/plugins/treesitter.lua
+++ lua/posterpole/highlighs/plugins/treesitter.lua
@@ -9,12 +9,13 @@ function M.setup(palette, config)
   return {
     ["@punctuation.bracket"] = { fg = colors.brightGray },
     ["@variable"] = { fg = colors.mainWhite },
-    ["@variable.member"] = { fg = colors.mainWhite },
+    ["@variable.member"] = { fg = colors.mainYellow },
     ["@function.call"] = { fg = colors.mainBlue, bold = true},
     ["@function"] = { fg = colors.mainBlue, bold = true},
     ["@property"] = { fg = colors.mainYellow },
 
-    ["@attribute.builtin.python"] = { link = "Macro" },
+    ["@type.python"] = { fg  = colors.mainPurple, bold=true },
+    ["@attribute.builtin.python"] = { fg = colors.mainPurple, bold=true },
     ["@module"] = { fg = colors.mainYellow },
 
     ["@function.macro.rust"] = { link = "Type" },
